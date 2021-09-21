0e2b5e08799729b6c366b478a8b42d91c6834826
D'Addona Santiago <54343046+Aseison@users.noreply.github.com>
2021-09-21 17:03:49 -0300

    juego ahorcado Tincho

--- Ejercicio3/Palabra.cs
+++ Ejercicio3/Palabra.cs
@@ -12,9 +12,9 @@ namespace Ejercicio3
          
         public Palabra()
         {
-            this.iDBPalabras = new string[] { "adaptacion"/*, "agudo", "antibiotico", "artritis", "ataxia", "atrofia","circulacion","cirugia","conducta","contractura",
+            this.iDBPalabras = new string[] { "adaptacion", "agudo", "antibiotico", "artritis", "ataxia", "atrofia","circulacion","cirugia","conducta","contractura",
                                                             "cronico", "cuadriplejia", "choquezuela", "diagnostico", "diplejia", "distrofia","dosis","espasticidad","espina","estimulacion",
-                                                            "evaluacion", "fisico", "fisioterapeuta", "hemiplejia", "herencia", "inflamacion","paraplejia","protesis","toxico","virus"*/};
+                                                            "evaluacion", "fisico", "fisioterapeuta", "hemiplejia", "herencia", "inflamacion","paraplejia","protesis","toxico","virus"};
         } 
         
         public string[] GetBaseDeDatos
